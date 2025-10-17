1e5637d999f117d70169d1409d3018151723f1ea
HOSSEINAJERLOO <ahosseinajerloo@gmail.com>
2025-10-17 07:50:49 -0700

    sync

--- app/Jobs/SendSMS.php
+++ app/Jobs/SendSMS.php
@@ -29,7 +29,7 @@ class SendSMS implements ShouldQueue
         $response = \Illuminate\Support\Facades\Http::timeout(100)->withHeaders([
             'ACCEPT' => 'application/json',
             'X-API-KEY' => 'LzJMD298QvvX8YSnrEECHEdewoi3qgcpFMbsKGAFwiYBO4TC'
-        ])->get('https://api.sms.ir/v1/receive/archive');
+        ])->get('https://api.sms.ir/v1/receive/latest');
         if ($response->status()) {
             $data = $response->json();
             if (isset($data['data']) and count($data['data']) > 0) {
@@ -56,7 +56,7 @@ class SendSMS implements ShouldQueue
                         if (!$warranty and !$giftCode)
                         {
                             $message='مشترک گرامی کد ارسال شده توسط شما نیاز به برسی بیشتر دارد باتشکر گروه بازرگانی چتر';
-//                            sendSMS($message,$item['mobile'],$sms);
+                            sendSMS($message,$item['mobile'],$sms);
                             continue;
                         }
                             if ($warranty)
@@ -65,10 +65,10 @@ class SendSMS implements ShouldQueue
                                 {
                                     $warranty->update(['phone_used'=>$item['mobile'],'used'=>1]);
                                     $message="مشتری گرامی گارانتی شما در تاریخ ".PHP_EOL.$today.PHP_EOL."فعال شد باتشکر گروه بازرگانی چتر";
-//                                    sendSMS($message,$item['mobile'],$sms);
+                                    sendSMS($message,$item['mobile'],$sms);
                                 }else{
                                     $message="مشتری گرامی گارانتی شما قبلا در تاریخ ".PHP_EOL.\Morilog\Jalali\Jalalian::forge($warranty->created_at).PHP_EOL."فعال شده است باتشکر گروه بازرگانی چتر";
-//                                    sendSMS($message,$item['mobile'],$sms);
+                                    sendSMS($message,$item['mobile'],$sms);
                                 }
                             }
 
@@ -82,7 +82,7 @@ class SendSMS implements ShouldQueue
                                         $charge=\App\Models\ChargeCode::where('used',0)->where('operator_id',$prefix->operator_id)->first();
                                         if ($charge){
                                             $message="مشتری گرامی کد شارژ شما ".PHP_EOL.$charge->copen.PHP_EOL."میباشد باتشکر گروه بازرگانی چتر";
-//                                            sendSMS($message,$item['mobile']);
+                                            sendSMS($message,$item['mobile']);
                                             $charge->update(['used'=>1,'phone_used'=>$item['mobile']]);
                                             $giftCode->update(['used'=>1,'phone_used'=>$item['mobile']]);
                                         }
@@ -90,7 +90,7 @@ class SendSMS implements ShouldQueue
                                     }
                                 }else{
                                     $message="مشتری گرامی کد شارژ شما تکراری میباشد  ".PHP_EOL."باتشکر گروه بازرگانی چتر";
-//                                    sendSMS($message,$item['mobile'],$sms);
+                                    sendSMS($message,$item['mobile'],$sms);
                                 }
                             }
                     }
